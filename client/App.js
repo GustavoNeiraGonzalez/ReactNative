62e967b0f1585cdb4000b16400e79184e803cb32
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-03-27 17:55:03 -0300

    agregada estilización a hola mundo e imagen contenedor

--- client/App.js
+++ client/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, View, StyleSheet, Image} from 'react-native'
 
 const App = () =>{
-  return <Text style={{fontSize:40}}>ola mundo</Text>
-}
+  return (
+  <View style={styles.container}>
+    <Text style={styles.title}>ola mundo</Text>
+    <Image style={styles.img} source={{uri:'https://picsum.photos/200/200/'}}></Image>
+  </View>
+  );
+};
+const styles = StyleSheet.create({
+  container:{flex:1, justifyContent:'center',alignItems:'center',backgroundColor:"#292929"},
+  title:{fontSize:40, color:'wheat'},
+  img:{height:200,width:200, borderRadius:20}
+})
 export default App;
